16da2ab0c7d651ae7f9fcd125c37d00ec57148e3
Danny Colombara <48296707+dcolombara@users.noreply.github.com>
2022-05-26 14:52:32 -0700

    essential filepaths for census pulse survey analysis

---
+++ 00_filepaths.R
@@ -0,0 +1,10 @@
+## Header ----
+# Author: Danny Colombara
+# Date: May 26, 2022
+# R version: 4.1.2
+# Purpose: set essential filepaths for all pulse survey analyses
+#
+# Needs rads >= v1.0.0
+#
+inputdir  <- "//PHDATA01/EPE_DATA/CDC COVID19 impacts eval/census_pulse/input/"
+outputdir <- "//PHDATA01/EPE_DATA/CDC COVID19 impacts eval/census_pulse/output/"
