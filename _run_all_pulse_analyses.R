70c38aabfa035feffd545a3a4c8e61cf091cd901
Danny Colombara <48296707+dcolombara@users.noreply.github.com>
2022-05-27 15:29:29 -0700

    Add _run_all_pulse_analyses.R and .gitignore

---
+++ _run_all_pulse_analyses.R
@@ -0,0 +1,16 @@
+# Purpose: run all PULSE data ----
+# Author: Danny Colombara
+# R verson: 4.1.2
+# Date: May 26, 2022
+# Purpose: Fresh run of all pulse survey analyses from one file
+
+base_filepath <- "https://raw.githubusercontent.com/PHSKC-APDE/pulse_survey/main/"
+
+source(paste0(base_filepath, "01_phase1_analysis.R"))
+source(paste0(base_filepath, "01_phase2_analysis.R"))
+source(paste0(base_filepath, "01_phase3_1_analysis.R"))
+source(paste0(base_filepath, "01_phase3_2_analysis.R"))
+source(paste0(base_filepath, "01_phase3_3_analysis.R"))
+source(paste0(base_filepath, "01_phase3_4_analysis.R"))
+
+
